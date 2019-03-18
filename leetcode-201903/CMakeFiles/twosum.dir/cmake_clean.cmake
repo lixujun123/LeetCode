d551a2cde88d51ca00c75284a261819ad37c83a7
lixujun <lixujun@forwardx.com>
2019-03-18 11:32:00 +0800

    add leetcode-201903

---
+++ leetcode-201903/CMakeFiles/twosum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/twosum.dir/0001-Two-Sum-main2.cpp.o"
+  "twosum.pdb"
+  "twosum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/twosum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
